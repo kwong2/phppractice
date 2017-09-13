8213b6c7b7ed6402e2fb2b002c2a6944f56a4413
Kevin Wong <k.wong4@yahoo.com>
2017-09-13 16:37:30 -0400

    first commit

--- ex1.php
+++ ex1.php
@@ -1,3 +1,5 @@
+<!-- Create a script that displays 1-2-3-4-5-6-7-8-9-10 on one line. There will be no hyphen(-) at starting and ending position. -->
+
 <?php
   for ($i = 0; $x <11; $x++){
     echo "$x" + -;
